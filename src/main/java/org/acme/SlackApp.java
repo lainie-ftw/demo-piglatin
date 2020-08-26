ee8d57b65aba2496ec666d8e9de812d33a2bf04f
Laine <39713102+lainie-ftw@users.noreply.github.com>
2020-08-26 11:48:34 -0400

    Add files via upload

---
+++ src/main/java/org/acme/SlackApp.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.servlet.SlackAppServlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/slack/events")
+public class SlackApp extends SlackAppServlet {
+  private static final long serialVersionUID = 1L;
+  public SlackApp() throws IOException { super(initSlackApp()); }
+  public SlackApp(App app) { super(app); }
+
+  private static App initSlackApp() throws IOException {
+    App app = new App();
+    app.command("/piglatin", (req, ctx) -> {
+      return ctx.ack("pig latin app reached!");
+    });
+    return app;
+  }
+}
