50d3eab5eee4b61ef983279173b36e3a395f65d9
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-06 13:39:10 -0400

    Create TranslationForwarder.java

---
+++ src/main/java/org/lj/TranslationForwarder.java
@@ -0,0 +1,21 @@
+package org.lj;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * A bean consuming data from the "prices" Kafka topic and applying some conversion.
+ * The result is pushed to the "my-data-stream" stream which is an in-memory stream.
+ */
+@ApplicationScoped
+public class TranslationForwarder {
+                                
+    @Outgoing("messaging-demo")                         
+    @Broadcast                                          
+    public String forward(String message) {
+        return message;
+    }
+
+}
