b4495a38e557a8cac6b3a4291c2a0ba851f49268
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-06 16:54:36 -0400

    Create PigLatin.java

---
+++ src/main/java/org/lj/PigLatin.java
@@ -0,0 +1,65 @@
+package org.lj;
+
+public class PigLatin ( 
+ //Pig Latin logic borrowed from here: http://pages.cs.wisc.edu/~ltorrey/cs302/examples/PigLatinTranslator.java
+ 
+  public String translateToPigLatin(String textToTranslate) {
+    String translatedText = "";
+    int i = 0;
+    while (i < textToTranslate.length()) {
+      // Take care of punctuation and spaces
+      while (i < textToTranslate.length() && !isLetter(textToTranslate.charAt(i))) {
+        translatedText = translatedText + textToTranslate.charAt(i);
+        i++;
+      }
+
+      // If there aren't any words left, stop.
+      if (i >= textToTranslate.length()) break;
+
+      // Otherwise we're at the beginning of a word.
+      int begin = i;
+      while (i<textToTranslate.length() && isLetter(textToTranslate.charAt(i))) {
+        i++;
+      }
+
+      // Now we're at the end of a word, so translate it.
+      int end = i;
+      translatedText = translatedText + pigWord(textToTranslate.substring(begin, end));
+  }
+  return translatedText;
+}
+
+  /**
+   * Method to test whether a character is a letter or not.
+   * @param c The character to test
+   * @return True if it's a letter
+   */
+  private boolean isLetter(char c) {
+    return ( (c >='A' && c <='Z') || (c >='a' && c <='z') );
+  }
+
+  /**
+   * Method to translate one word into pig latin.
+   * @param word The word in english
+   * @return The pig latin version
+   */
+  private String pigWord(String word) {
+    int split = firstVowel(word);
+    return word.substring(split)+"-"+word.substring(0, split)+"ay";
+  }
+
+  /**
+   * Method to find the index of the first vowel in a word.
+   * @param word The word to search
+   * @return The index of the first vowel
+   */
+  private int firstVowel(String word) {
+    word = word.toLowerCase();
+    for (int i=0; i<word.length(); i++)
+      if (word.charAt(i)=='a' || word.charAt(i)=='e' ||
+          word.charAt(i)=='i' || word.charAt(i)=='o' ||
+          word.charAt(i)=='u')
+        return i;
+    return 0;
+  }
+}
