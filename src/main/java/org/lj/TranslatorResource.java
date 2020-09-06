cb70a3210719b017da5daec5e0cc8a5e90fcc82d
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-06 16:50:27 -0400

    Create TranslatorResource.java

---
+++ src/main/java/org/lj/TranslatorResource.java
@@ -0,0 +1,19 @@
+package org.lj
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+public class TranslatorResource {
+
+    @Inject @Channel("messaging-demo") Emitter<String> translationEmitter;
+
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void addTranslation(String translation) {
+       translationEmitter.send(translation);
+    }
+}
