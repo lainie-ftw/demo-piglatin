dd9372cc5e8ffbb637f64e190400e5768ba57072
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-07 14:56:29 -0400

    Update SlackApp.java

--- src/main/java/org/lj/SlackApp.java
+++ src/main/java/org/lj/SlackApp.java
@@ -29,22 +29,23 @@ public class SlackApp extends SlackAppServlet {
     App app = new App();
     app.command("/piglatin", (req, ctx) -> {
       
+      ProcessSlackRequest.process(req);
       //Translate the input text and set up the message
-      PigLatin pigLatin = new PigLatin();
-      String textToTranslate = req.getPayload().getText();
-      SlackMessage message = new SlackMessage();
-      message.text = pigLatin.translateToPigLatin(textToTranslate);
-      LOG.info(textToTranslate + " translated to " + message.text);
+  //    PigLatin pigLatin = new PigLatin();
+    //  String textToTranslate = req.getPayload().getText();
+      //SlackMessage message = new SlackMessage();
+      //message.text = pigLatin.translateToPigLatin(textToTranslate);
+      //LOG.info(textToTranslate + " translated to " + message.text);
       
       //Send result to Kafka
-      kafkaSender.send(message);
+  //    kafkaSender.send(message);
       
 //      TranslatorResource translator;
   //    translator.text = translatedText;
     //  translator.addTranslation();
       
       //Tell Slack we got the message.
-      return ctx.ack(message.text);
+      return ctx.ack("sdfidk");
     });
     return app;
   }
