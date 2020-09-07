e8f3e4e40fbcd8d54aca06cf79b9348b2b5485eb
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-07 13:45:36 -0400

    Create SlackMessage.java

---
+++ src/main/java/org/lj/SlackMessage.java
@@ -0,0 +1,13 @@
+package org.lj;
+
+public class SlackMessage {
+    
+    public String text;
+    
+    public void SlackMessage() {
+    }
+    
+    public void SlackMessage(String text) {
+      this.text = text;
+    }
+}
