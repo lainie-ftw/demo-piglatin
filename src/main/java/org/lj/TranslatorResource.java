bef090099871fa4feb87ce6d1505d65ac6a72960
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-06 20:10:09 -0400

    Update TranslatorResource.java

--- src/main/java/org/lj/TranslatorResource.java
+++ src/main/java/org/lj/TranslatorResource.java
@@ -5,17 +5,15 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import org.jboss.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Path;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
 
+@ApplicationScoped
 public class TranslatorResource {
     private static final Logger LOG = Logger.getLogger(TranslatorResource.class);
 
     @Inject @Channel("messaging-demo") Emitter<String> translationEmitter;
 
-//    @Consumes(MediaType.TEXT_PLAIN)
     public void addTranslation(String translation) {
        LOG.info("incoming translated text is " + translation);
        translationEmitter.send(translation);
