a1047a6aa6f5505e84f65402e631f4830669ab1e
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-07 14:06:06 -0400

    Update SlackMessage.java

--- src/main/java/org/lj/SlackMessage.java
+++ src/main/java/org/lj/SlackMessage.java
@@ -4,9 +4,6 @@ public class SlackMessage {
     
     public String text;
     
-    public void SlackMessage() {
-    }
-    
     public void SlackMessage(String text) {
       this.text = text;
     }
