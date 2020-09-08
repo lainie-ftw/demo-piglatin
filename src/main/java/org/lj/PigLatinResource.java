c4c4cb544b93bb3e8f028bddbedcde76fedb03b7
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-08 17:20:59 -0400

    Update PigLatinResource.java

--- src/main/java/org/lj/PigLatinResource.java
+++ src/main/java/org/lj/PigLatinResource.java
@@ -2,6 +2,7 @@ package org.lj;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -17,7 +18,7 @@ public class PigLatinResource {
     public PigLatinResource() {
     }
 
-    @GET
+    @POST
     public PigLatin translate(PigLatin input) {
         pigLatin.translateToPigLatin(input.translatedText);
         return pigLatin;
