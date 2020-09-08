d4e003772087576bc0a942ee4f10aaf75bac31ce
Laine Vyvyan <39713102+lainie-ftw@users.noreply.github.com>
2020-09-08 11:54:59 -0400

    Update KafkaHelper.java

--- src/main/java/org/lj/KafkaHelper.java
+++ src/main/java/org/lj/KafkaHelper.java
@@ -7,8 +7,7 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 import javax.inject.Inject;
 
 public class KafkaHelper {
-  
-  @Inject 
+   
   @Channel("messaging-demo") 
   Emitter<SlackMessage> kafkaSender;
 
