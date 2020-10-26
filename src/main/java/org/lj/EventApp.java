3cf3ce70a93d61cadc0362c3fdf04fcedb835d29
Laine Minor <39713102+lainie-ftw@users.noreply.github.com>
2020-10-26 11:56:50 -0400

    Update EventApp.java

--- src/main/java/org/lj/EventApp.java
+++ src/main/java/org/lj/EventApp.java
@@ -7,11 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
-import javax.inject.Inject;
-
-import java.io.InputStream;
 
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
@@ -19,43 +17,23 @@ import java.io.PrintWriter;
 
 @WebServlet("/events")
 public class EventApp extends HttpServlet{
-  private static final long serialVersionUID = 1L;
-  
-  private static final Logger LOG = Logger.getLogger(SlackApp.class);
+	
+//  private static final long serialVersionUID = 1L;
+  private static final Logger LOG = Logger.getLogger(EventApp.class);
   
   @Inject
   @Channel("slack")
   Emitter<PigLatin> slackEmitter;
 
   @Override
-  protected void doPost(HttpServletRequest request, 
-  HttpServletResponse response)
-	      throws ServletException, IOException {
-      //    InputStream  is = request.getInputStream();
-      //    StringBuilder sb = new StringBuilder();
-
-      //    int i;
-      //    char c;
-      //    while((i = is.read())!=-1) {
-         
-            // converts integer to character
-       //     c = (char)i;
-       //     sb.append(c);
-            
-            // prints character
-           // System.out.print(c);
-       //  }
-       //   LOG.info(sb.toString());
-          PrintWriter writer = response.getWriter();    
-	  
-	 // JsonObject json = new JsonObject(request.getParameterMap());
-	  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {    
 	  String inputText = request.getParameter("text");
-
-          PigLatin pigLatin = new PigLatin(inputText);
-          pigLatin.translateToPigLatin();
-          slackEmitter.send(pigLatin);
+	  PigLatin pigLatin = new PigLatin(inputText);
+	  pigLatin.translateToPigLatin();
+	  slackEmitter.send(pigLatin);
+	 
+	  PrintWriter writer = response.getWriter();   
 	  writer.print(pigLatin.outputText);
-          writer.close();
+	  writer.close();
   }
 }
